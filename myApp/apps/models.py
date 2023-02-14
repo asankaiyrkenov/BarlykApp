604067d45284ed83916d834ccbd9517fe9ae3c7d
asankaiyrkenov <82355525+asankaiyrkenov@users.noreply.github.com>
2023-02-14 10:25:05 +0600

    Update models
    hgfhchvjklgjkhmvcbnjkkh

--- myApp/apps/models.py
+++ myApp/apps/models.py
@@ -1,3 +1,23 @@
+from __future__ import absolute_import
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    fullname = models.TextField(max_length=100)
+    password = models.TextField()
+    email = models.TextField()
+    number = models.TextField()
+    liked = models.ManyToManyField('Product', related_name='liked_by')
+
+class Product(models.Model):
+    title = models.TextField()
+    text = models.TextField()
+    price = models.TextField()
+    image = models.TextField()
+    states = models.TextField()
+    city = models.TextField()
+    isDeliver = models.TextField()
+    seller = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, related_name='liked_products')
+    cartype = models.TextField()
+    likes = models.ManyToManyField(Person, related_name='likes')
+    views = models.ManyToManyField(Person, related_name='views')
